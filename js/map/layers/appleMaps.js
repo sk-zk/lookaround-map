baccb897431b5da8a453f2498ce07a5fe8e54d09
sk-zk <sk-zk@users.noreply.github.com>
2022-12-06 23:30:31 +0100

    Fetch Apple road tiles from all subdomains

--- js/map/layers/appleMaps.js
+++ js/map/layers/appleMaps.js
@@ -1,3 +1,5 @@
+const subdomains = ["1", "2", "3", "4"];
+
 L.TileLayer.AppleMapsTiles = L.TileLayer.extend({
     initialize: function (auth, opts) {
       this.auth = auth;
@@ -11,19 +13,20 @@ L.TileLayer.AppleMapsTiles = L.TileLayer.extend({
           L.DomEvent.on(tile, "error", L.Util.bind(this._tileOnError, this, done, tile));
   
       let url = "";
+      const subdomain = subdomains[(Math.random() * subdomains.length) | 0];
       switch (this.options.type) {
         default:
         case "road":
-          url = `https://cdn3.apple-mapkit.com/ti/tile?` +
+          url = `https://cdn${subdomain}.apple-mapkit.com/ti/tile?` +
           `style=0&size=1&x=${coords.x}&y=${coords.y}&z=${coords.z}&v=2210195&scale=1` +
           `&lang=en&poi=1&tint=${this.options.tint}&emphasis=standard`;
           break;
         case "satellite":
-          url = `https://sat-cdn1.apple-mapkit.com/tile?` +
+          url = `https://sat-cdn${subdomain}.apple-mapkit.com/tile?` +
                 `style=7&size=1&scale=1&x=${coords.x}&y=${coords.y}&z=${coords.z}&v=9312`;
           break;
         case "satellite-overlay":
-          url = `https://cdn1.apple-mapkit.com/ti/tile?` +
+          url = `https://cdn${subdomain}.apple-mapkit.com/ti/tile?` +
                 `style=46&size=1&x=${coords.x}&y=${coords.y}&z=${coords.z}&scale=1&poi=0`;
           break;
       }
