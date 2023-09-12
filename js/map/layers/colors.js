f82c645ca97daf4b3b0fdaee41ee264731b1ec11
sk-zk <sk-zk@users.noreply.github.com>
2023-09-12 13:46:11 +0200

    Update max date

--- js/map/layers/colors.js
+++ js/map/layers/colors.js
@@ -2,7 +2,7 @@ import { interpolateTurbo } from "d3-scale-chromatic";
 import { LineColorType, CoverageType } from "../../enums.js";
 
 const earliestDate = Math.floor(new Date("2018-06-01").getTime());
-const latestDate =   Math.floor(new Date("2023-06-01").getTime());
+const latestDate =   Math.floor(new Date("2023-09-01").getTime());
 
 const carLineColor = "rgba(26, 159, 176, 1)";
 const trekkerLineColor = "rgba(173, 140, 191, 1)";
