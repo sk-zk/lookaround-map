cbdb0d218f02655797f7f2caa2276477a9fe2a6d
sk-zk <sk-zk@users.noreply.github.com>
2024-09-14 17:33:35 +0200

    Bump max date and Build ID for colors and filters

--- js/map/layers/colors.js
+++ js/map/layers/colors.js
@@ -4,7 +4,7 @@ import { FilterSettings } from "../FilterSettings.js";
 import { interpolateTurbo } from "d3-scale-chromatic";
 
 const earliestDate = Math.floor(new Date("2018-06-01").getTime());
-const latestDate =   Math.floor(new Date("2024-06-01").getTime());
+const latestDate =   Math.floor(new Date("2024-08-01").getTime());
 
 const carLineColor = "rgba(26, 159, 176, 1)";
 const trekkerLineColor = "rgba(173, 140, 191, 1)";
@@ -62,7 +62,7 @@ class CoverageColorer {
       this.colorFunction = (metadata) => interpFn(offsetFn(convFn(metadata.timestamp)));
     } else if (filterSettings.lineColorType === LineColorType.BuildId) {
       const start = 511228947; // lowest value discovered since I started scraping
-      const end = 2100000000; // highest value plus some breathing room
+      const end = 2200000000; // highest value plus some breathing room
       const convFn = createValueToPercentFunction(start, end);
       const offsetFn = offsetTurbo;
       const interpFn = interpolateTurbo;
