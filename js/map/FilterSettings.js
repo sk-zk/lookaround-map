c0a25d734e2674a4c68138ca5f34b80998814de7
sk-zk <sk-zk@users.noreply.github.com>
2025-01-28 17:05:09 +0100

    Bump max date for colors and filters

--- js/map/FilterSettings.js
+++ js/map/FilterSettings.js
@@ -3,7 +3,7 @@ import { LineColorType } from "../enums";
 const defaults = {
     filterByDate: false,
     minDate: Math.floor(new Date("2018-06-01").getTime()),
-    maxDate: Math.floor(new Date("2024-10-01").getTime()),
+    maxDate: Math.floor(new Date("2024-12-01").getTime()),
     showCars: true,
     showTrekkers: true,
     polygonFilter: null,
